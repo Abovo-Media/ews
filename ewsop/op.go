b2cc8785bd69ae3eaba7ca3e94a9c6475d8b77fc
roel.schut <roel.schut@abovo.nl>
2023-04-06 16:38:00 +0200

    ewsop.GetOperation now returns an OperationContext struct

--- ewsop/op.go
+++ ewsop/op.go
@@ -2,19 +2,31 @@ package ewsop
 
 import (
 	"context"
+
+	"github.com/Abovo-Media/go-ews"
 )
 
 type Operation string
 
 func (op Operation) String() string { return string(op) }
 
+type OperationContext struct {
+	Operation Operation
+	RequestId string
+}
+
 type operationKey struct{}
 
 func setOperation(ctx context.Context, op Operation) context.Context {
-	return context.WithValue(ctx, operationKey{}, op)
+	return context.WithValue(ctx, operationKey{}, OperationContext{
+		Operation: op,
+	})
 }
 
-func GetOperation(ctx context.Context) (Operation, bool) {
-	v, ok := ctx.Value(operationKey{}).(Operation)
+func GetOperation(ctx context.Context) (OperationContext, bool) {
+	v, ok := ctx.Value(operationKey{}).(OperationContext)
+	if v.RequestId == "" {
+		v.RequestId, _ = ews.RequestId(ctx)
+	}
 	return v, ok
 }
